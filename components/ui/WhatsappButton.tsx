4e02c425642ed1d96811373f7d313ae30f988069
seyitbugraerden <seyitbugraerden@gmail.com>
2025-02-07 00:52:32 +0300

    whatsapp fix

--- components/ui/WhatsappButton.tsx
+++ components/ui/WhatsappButton.tsx
@@ -7,6 +7,7 @@ const WhatsappButton = () => {
     <Link
       href="http://wa.me/+905323445502"
       target="_blank"
+      aria-label="Whatsapp İletişim"
       className="fixed right-6 bottom-6 bg-[#38E759] rounded-xl p-2 z-[9999999] text-white hover:!text-white shadow-md shadow-black/30 hover:brightness-90 transition duration-200"
     >
       <FaWhatsapp size={42} />
